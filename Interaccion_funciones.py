1ddfc72004bac35da98f50ea5b52f190db17ea47
CarlosRa10 <carloseduardoramirez10@gmail.com>
2024-06-11 14:16:10 -0400

    subir mas ejercicio

--- Interaccion_funciones.py
+++ Interaccion_funciones.py
@@ -44,3 +44,87 @@ def evaluar_jugada(dado1,dado2):
 dado1, dado2 = lanzar_dados()
 resultado = evaluar_jugada(dado1,dado2)
 print(resultado)
+
+
+
+lista_numeros = [1,2,15,7,2,8]
+ 
+def reducir_lista(lista):
+    lista = list(set(lista))
+    lista.sort()
+    lista.pop(-1)
+    return lista
+ 
+def promedio(lista):
+    valor_medio = sum(lista)/len(lista)
+    return valor_medio
+
+
+lista_numeros = [1,2,15,7,2]
+
+def reducir_lista(lista):
+    lista = list(set(lista))
+    lista.sort()
+    lista.pop()
+    return lista
+        
+def promedio(lista):
+    valor_promedio = sum(lista)/len(lista)
+    return valor_promedio
+
+eliminando_duplicados = reducir_lista(lista_numeros)
+resultado_promedio = promedio(eliminando_duplicados)
+print(eliminando_duplicados)
+print(resultado_promedio)
+
+
+
+lista_numeros = [1,2,15,7,2,8]
+ 
+import random
+ 
+def lanzar_moneda():
+    resultado = random.choice(["Cara", "Cruz"])
+    return resultado
+ 
+def probar_suerte(moneda, lista):
+    if moneda == "Cara":
+        print("La lista se autodestruirá")
+        return []
+    elif moneda == "Cruz":
+        print("La lista fue salvada")
+        return lista
+    
+    
+from random import * 
+lista_numeros = [1,2,3,4,5,6,7,8,9,10]
+
+def lanzar_moneda():
+    resultado = choice(['Cara','Cruz'])
+    return resultado
+def probar_suerte(moneda,lista):
+    if moneda == 'Cara':
+        print('La lista se autodestruirá')
+        return []
+    else:
+        print('La lista fue salvada')
+        return lista
+    
+moneda_lanzada = lanzar_moneda()
+probando_suerte = probar_suerte(moneda_lanzada,lista_numeros)
+print(probando_suerte)
+
+
+
+def lanzar_moneda():
+    resultado = choice(['Cara','Sello'])
+    return resultado
+
+def verificar_moneda(moneda):
+    if moneda == 'Cara':
+        print('Salio Cara')
+    else:
+        print('Salio Sello')
+
+resultado_de_lanzar = lanzar_moneda()
+resultado_final = verificar_moneda(resultado_de_lanzar)
